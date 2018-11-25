b861cad61859e79f91ad33eafc5b62739c6e61db
SaraPalacios <saris_pal@hotmail.com>
2018-11-25 13:09:18 -0500

    Finalizando funcion que compara

--- codigos/Comparar_Producto.py
+++ codigos/Comparar_Producto.py
@@ -5,16 +5,54 @@ Created on SAT Nov 24 2018
 @author: Sara Palacios
 """
 import tkinter as tk
+import re
 from clase import *
 from Agregar_Nuevo_p import entradas
 
+
+def texto():
+    a = selección.get()
+    b = entry1.get()
+    c = entry2.get()
+    book = Excel("wb8.xlsx", "Hoja1")
+    txtb = book.read_wb(b)
+    txtc = book.read_wb(c)
+    txtbnums = book.read_numbs(b)
+    
+    window=tk.Tk()
+    window.title("Informcación específica y comparación")
+    texto = tk.Text(master=window, height=10, width=70)
+    texto.grid(column=0, row=1)
+    for i in range(len(variables)):
+        if a == variables[i]:
+            indicemenu = variables.index(variables[i])
+            indiceexcelb = txtb[indicemenu+3]
+            indiceexcelc = txtc[indicemenu+3]
+            numsb=re.sub("\D", "", indiceexcelb)
+            numsc=re.sub("\D", "", indiceexcelc)
+            comparación = min(numsb, numsc)
+            if comparación == numsb:
+                msg="El producto más saludable con respecto a {0} es: {1}".format(a,b) + "\n" + "Con: {0}".format(indiceexcelb)
+                msg1="El producto menos saludable con respecto a {0} es: {1}".format(a,c) + "\n" + "Con: {0}".format(indiceexcelc)
+                texto.insert(tk.END, ("\n" + msg + "\n" + msg1))
+            elif comparación == numsc:
+                msg2="El producto más saludable con respecto a {0}, es: {1}".format(a,c) +"\n" + "con: {0}".format(indiceexcelc)
+                msg3="El producto menos saludable con respecto a {0} es: {1}".format(a,b) + "\n" + "Con: {0}".format(indiceexcelb)
+                texto.insert(tk.END, ("\n" + msg2 + "\n" + msg3))
+            
+                             
+            
+            
+            
+            
+            
 
 
 """Esta función crea la ventana que permite comparar productos,
     se activa al dar cick en: 'Comparar Producto' """
 #---Ventana 'Analizar producto'----
 otherwn = tk.Tk()
-otherwn.title("Analizar Producto")
+otherwn.title("Comparar Producto")
 otherwn.geometry("500x400")
 
 #---Etiqueta principal---
@@ -46,99 +84,24 @@ entry2 = tk.Entry(master=otherwn)
 entry2.grid(column=0, row=4, padx=330, pady=10,sticky="W")
 palabra2 = entry1.get()
 
-#---vars de CheckButtons---
-peso=tk.IntVar()
-tamaño=tk.IntVar()
-cals=tk.IntVar()
-calsgras=tk.IntVar()
-grassat=tk.IntVar()
-grastrans=tk.IntVar()
-col=tk.IntVar()
-sod=tk.IntVar()
-fib=tk.IntVar()
-azucar=tk.IntVar()
-vita=tk.IntVar()
-vitc=tk.IntVar()
-vitd=tk.IntVar()
-vite=tk.IntVar()
-cal=tk.IntVar()
-hierro=tk.IntVar()
 
-#---Checkbuttons---
-chkp=tk.Checkbutton(otherwn, text="Peso Neto", font=("Malgun Gothic Semilight", 10),
-                    variable=peso, onvalue=1, offvalue=0)
-chkp.grid(column=0, row=7, padx=50, sticky="W")
-chkt=tk.Checkbutton(otherwn, text="Tamaño por porción", font=("Malgun Gothic Semilight", 10),
-                    variable=tamaño, onvalue=1, offvalue=0)
-chkt.grid(column=0, row=8, padx=50, sticky="W")
-chkcals=tk.Checkbutton(otherwn, text="Calorías", font=("Malgun Gothic Semilight", 10),
-                    variable=cals, onvalue=1, offvalue=0)
-chkcals.grid(column=0, row=9,padx=50, sticky="W")
-chkcg=tk.Checkbutton(otherwn, text="Calorías de grasa", font=("Malgun Gothic Semilight", 10),
-                    variable=calsgras, onvalue=1, offvalue=0)
-chkcg.grid(column=0, row=10, padx=50,sticky="W")
-chkgs=tk.Checkbutton(otherwn, text="Grasa Saturada", font=("Malgun Gothic Semilight", 10),
-                    variable=grassat, onvalue=1, offvalue=0)
-chkgs.grid(column=0, row=11, padx=50,sticky="W")
-chkgt=tk.Checkbutton(otherwn, text="Grasas Trans", font=("Malgun Gothic Semilight", 10),
-                    variable=grastrans, onvalue=1, offvalue=0)
-chkgt.grid(column=0, row=12, padx=50,sticky="W")
-chkcol=tk.Checkbutton(otherwn, text="Colesterol", font=("Malgun Gothic Semilight", 10),
-                    variable=col, onvalue=1, offvalue=0)
-chkcol.grid(column=0, row=13,padx=50, sticky="W")
-chksod=tk.Checkbutton(otherwn, text="Sodio", font=("Malgun Gothic Semilight", 10),
-                    variable=sod, onvalue=1, offvalue=0)
-chksod.grid(column=0, row=14,padx=50, sticky="W")
+#--lista de Optionmenu---
+variables =["Peso Neto", "Tamaño por porción", "Calorías", "Calorías de grasa",
+            "Grasa Saturada",  "Grasas Trans", "Colesterol", "Sodio", "Fibra",
+            "Azúcares", "Vitamina A", "Vitamina C", "Vitamina D", "Vitamina E",
+            "Calcio", "Hierro"]
+#---Optionmenu---
+selección = tk.StringVar()
+selección.set("Seleccione una opción")
 
-chkfib=tk.Checkbutton(otherwn, text="Fibra", font=("Malgun Gothic Semilight", 10),
-                    variable=fib, onvalue=1, offvalue=0)
-chkfib.grid(column=0, row=7, padx=250, sticky="W")
-chkazuc=tk.Checkbutton(otherwn, text="Azúcares", font=("Malgun Gothic Semilight", 10),
-                    variable=azucar, onvalue=1, offvalue=0)
-chkazuc.grid(column=0, row=8, padx=250, sticky="W")
-chkvita=tk.Checkbutton(otherwn, text="Vitamina A", font=("Malgun Gothic Semilight", 10),
-                    variable=vita, onvalue=1, offvalue=0)
-chkvita.grid(column=0, row=9, padx=250, sticky="W")
-chkvitc=tk.Checkbutton(otherwn, text="Vitamina C", font=("Malgun Gothic Semilight", 10),
-                    variable=vitc, onvalue=1, offvalue=0)
-chkvitc.grid(column=0, row=10, padx=250, sticky="W")
-chkvitd=tk.Checkbutton(otherwn, text="Vitamina D", font=("Malgun Gothic Semilight", 10),
-                    variable=vitd, onvalue=1, offvalue=0)
-chkvitd.grid(column=0, row=11, padx=250, sticky="W")
-chkvite=tk.Checkbutton(otherwn, text="Vitamina E", font=("Malgun Gothic Semilight", 10),
-                    variable=vite, onvalue=1, offvalue=0)
-chkvite.grid(column=0, row=12, padx=250, sticky="W")
-chkcalcio=tk.Checkbutton(otherwn, text="Calcio", font=("Malgun Gothic Semilight", 10),
-                    variable=cal, onvalue=1, offvalue=0)
-chkcalcio.grid(column=0, row=13, padx=250, sticky="W")
-chkhierro=tk.Checkbutton(otherwn, text="Hierro", font=("Malgun Gothic Semilight", 10),
-                    variable=hierro, onvalue=1, offvalue=0)
-chkhierro.grid(column=0, row=14, padx=250, sticky="W" )
+om = tk.OptionMenu(otherwn, selección,  *variables)
+
+om.grid(column=0, row=7, padx=150, sticky="W")
+
+#---Boton---
+but1=tk.Button(master=otherwn, text="Buscar información", command=texto)
+but1.grid(column=0, row=18, pady= 20,sticky="W")
 
 
+otherwn.mainloop()
 
-#--Botón para buscar--
-but1=tk.Button(master=otherwn, text="Buscar información",
-               command= lambda: asd(lista_checks))
-but1.grid(column=0, row=18, padx=300, pady= 20,sticky="W")
-
-lista_checks=[peso,tamaño,cals, calsgras, grassat, grastrans,
-              col, sod, fib, azucar, vita, vitc, vitd,
-              vite, cal, hierro]
-
-
-
-otherwn.mainloop
-
-def asd(lista):
-    lst=entradas(lista)
-    temp = []
-    for i in range(len(lst)):
-        c = lst.index(lst[i])
-        print(c)
-        
-            
-    
-
-
-    
